87685593fb432e3826c131351a710a0c482f0881
Tomas Rodriguez <federodriguez@me.com>
2024-10-06 09:12:40 -0500

    Add: Ball launcher crudely functional

---
+++ Painball/Assets/Scripts/BoardObjectScripts/LaunchZone.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class LaunchZone : MonoBehaviour
+{
+    private BoxCollider boxCollider;
+    public Transform launchPointer;
+    public float maxForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    private Rigidbody CheckForBallInZone()
+    {
+        // Get the center and size of the BoxCollider
+        Vector3 boxCenter = boxCollider.transform.position + boxCollider.center;
+        Vector3 boxSize = boxCollider.size / 2;
+
+        // Detect all colliders inside the BoxCollider
+        Collider[] colliders = Physics.OverlapBox(boxCenter, boxSize, boxCollider.transform.rotation);
+
+        // Process the detected colliders
+        foreach (Collider collider in colliders)
+        {
+            if(collider.gameObject.tag == "Ball"){
+                return collider.gameObject.GetComponent<Rigidbody>();
+            }
+        }
+        return null;
+    }
+
+    public void Launch(float charge){
+        Rigidbody ball = CheckForBallInZone();
+        if(ball==null){
+            print("No Balls");
+            //No ball detected in launch zone
+            return;
+        }
+        print("Launch");
+        //calculate desired launch direction
+        Vector3 direction = launchPointer.position - this.transform.position;
+        direction = direction.normalized;
+
+        //Calculate force
+        direction *= maxForce;
+        direction *= charge;
+
+        //Exert force on ball
+        ball.AddForce(direction, ForceMode.Impulse);
+    }
+    
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Launch(1);
+            
+        }
+    }
+}
