e817a6f9c7b8c982e74142dec8f34614fdd39607
SeanAlt <gamemastergross@gmail.com>
2024-10-10 15:46:32 -0500

    Update FlipperControl.cs

--- Painball/Assets/Scripts/BoardObjectScripts/FlipperControl.cs
+++ Painball/Assets/Scripts/BoardObjectScripts/FlipperControl.cs
@@ -17,13 +17,18 @@ public class FlipperControl : MonoBehaviour
     {
         JointMotor2D motor = hinge.motor;
 
+        
         if (Input.GetKey(key))
         {
             motor.motorSpeed = force; // Move the flipper when key is pressed
         }
-        else
+        else if (hinge.angle > 0) // Adjust this condition based on your flipper's design
         {
             motor.motorSpeed = -force; // Move back to the resting position
+        }
+        else
+        {
+            motor.motorSpeed = 0; // Stop the motor when in resting position
         }
 
         hinge.motor = motor;
