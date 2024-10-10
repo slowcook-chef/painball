ff01fb03c25a517c7ae1e7001e3ecff9ce59b578
SeanAlt <gamemastergross@gmail.com>
2024-10-10 14:34:28 -0500

    Update FlipperControl.cs

--- Painball/Assets/Scripts/BoardObjectScripts/FlipperControl.cs
+++ Painball/Assets/Scripts/BoardObjectScripts/FlipperControl.cs
@@ -6,27 +6,26 @@ public class FlipperControl : MonoBehaviour
 {
     public KeyCode key = KeyCode.Z; // Default key is Z for the left flipper
     public float force = 500f;
-    private HingeJoint hinge;
+    private HingeJoint2D hinge;
 
     void Start()
     {
-        hinge = GetComponent<HingeJoint>();
+        hinge = GetComponent<HingeJoint2D>();
     }
 
     void Update()
     {
-        JointMotor motor = hinge.motor;
+        JointMotor2D motor = hinge.motor;
 
         if (Input.GetKey(key))
         {
-            motor.targetVelocity = force; // Move the flipper when key is pressed
+            motor.motorSpeed = force; // Move the flipper when key is pressed
         }
         else
         {
-            motor.targetVelocity = -force; // Move back to the resting position
+            motor.motorSpeed = -force; // Move back to the resting position
         }
 
         hinge.motor = motor;
     }
 }
-
