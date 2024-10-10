c330f53c0c996a41324ba32f701d4a473fb626cc
SeanAlt <gamemastergross@gmail.com>
2024-10-10 15:58:28 -0500

    Update FlipperControl.cs

--- Painball/Assets/Scripts/BoardObjectScripts/FlipperControl.cs
+++ Painball/Assets/Scripts/BoardObjectScripts/FlipperControl.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class FlipperControl : MonoBehaviour
 {
     public KeyCode key = KeyCode.Z; // Default key is Z for the left flipper
-    public float force = 500f;
+    public float launchForce = 500f; // Force when flipping
+    public float restingAngle = 0f; // The angle where the flipper rests
+    public float maxFlipAngle = 45f; // Maximum angle the flipper can flip to
+
     private HingeJoint2D hinge;
 
     void Start()
@@ -16,19 +19,19 @@ public class FlipperControl : MonoBehaviour
     void Update()
     {
         JointMotor2D motor = hinge.motor;
+        float currentAngle = hinge.angle;
 
-        // Check if the flipper is in the resting position
-        if (Input.GetKey(key))
+        if (Input.GetKey(key) && currentAngle < maxFlipAngle) // Only flip if under max angle
         {
-            motor.motorSpeed = force; // Move the flipper when key is pressed
+            motor.motorSpeed = launchForce; // Flip when key is pressed
         }
-        else if (transform.rotation.eulerAngles.z > 0) // Check the angle using the transform's rotation
+        else if (currentAngle > restingAngle) // Move back to resting position
         {
-            motor.motorSpeed = -force; // Move back to the resting position
+            motor.motorSpeed = -launchForce; // Move back to the resting position
         }
         else
         {
-            motor.motorSpeed = 0; // Stop the motor when in resting position
+            motor.motorSpeed = 0; // Stop the motor when at resting position
         }
 
         hinge.motor = motor;
